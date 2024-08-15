30ea52e4a5781d8db9efdfab53cd1f8ba0ffc16d
MK <melihk0705@gmail.com>
2024-08-15 22:45:57 +0300

    Sorting

--- basics/bsStoreApp/Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
+++ basics/bsStoreApp/Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+361ad3d2269c099c29c1f4734b0dc97179a10268")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+969f37e3d32a6afe0c883091bfaac7a331c67d1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
