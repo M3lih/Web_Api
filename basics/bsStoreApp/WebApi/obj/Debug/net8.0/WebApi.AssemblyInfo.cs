356d2d0ef430c38929660fe7bc86e4a0d03af742
MK <melihk0705@gmail.com>
2024-08-21 16:44:42 +0300

    Root Documentation

--- basics/bsStoreApp/WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ basics/bsStoreApp/WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2483b03bf4e133e1c024011dbea3372c274d3084")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60cf5cc6a3d4b4ea381864a5ef2edd150e24729d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
