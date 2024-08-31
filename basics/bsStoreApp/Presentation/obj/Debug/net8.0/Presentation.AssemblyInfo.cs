7a484d1fa2e31bd29db49399a7e0c1d61ce741cd
MK <melihk0705@gmail.com>
2024-08-31 21:50:21 +0300

    Belgelendirme

--- basics/bsStoreApp/Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
+++ basics/bsStoreApp/Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+906ce5c45710e868e06948e38d63331f37f1bade")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f684223d55ad11c052528c1652af765ab9e86164")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
